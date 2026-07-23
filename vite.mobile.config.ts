6e5e3a15c72725b31873d89be7bd5e0c9a823b6e
alexandramiart-design <alexandramiart@gmail.com>
2026-07-23 12:18:44 +0200

    Update vite.mobile.config.ts

---
+++ vite.mobile.config.ts
@@ -0,0 +1,16 @@
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  root: ".",
+  plugins: [react(), tailwindcss(), tsConfigPaths()],
+  build: {
+    rollupOptions: {
+      input: "mobile/index.html",
+    },
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+});
