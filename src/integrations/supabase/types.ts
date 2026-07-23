ad39f1e8b59662ef96449cd5948ca5ce8beab3d5
alexandramiart-design <alexandramiart@gmail.com>
2026-07-23 12:18:34 +0200

    Update src/integrations/supabase/types.ts

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -59,6 +59,33 @@ export type Database = {
         }
         Relationships: []
       }
+      web_messages: {
+        Row: {
+          content: string
+          created_at: string
+          id: string
+          images: Json
+          role: string
+          user_id: string
+        }
+        Insert: {
+          content?: string
+          created_at?: string
+          id?: string
+          images?: Json
+          role: string
+          user_id: string
+        }
+        Update: {
+          content?: string
+          created_at?: string
+          id?: string
+          images?: Json
+          role?: string
+          user_id?: string
+        }
+        Relationships: []
+      }
     }
     Views: {
       [_ in never]: never
